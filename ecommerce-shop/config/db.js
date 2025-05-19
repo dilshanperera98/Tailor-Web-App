0ad2d9f0c992be0b0d7234a76c502a20ec5a8e89
Dilshan Perera <83137780+dilshanperera98@users.noreply.github.com>
2025-05-19 13:51:17 +0530

    Fix formatting by adding a newline at the end of db.js

--- ecommerce-shop/config/db.js
+++ ecommerce-shop/config/db.js
@@ -6,4 +6,4 @@ mongoose.connect('mongodb+srv://nawarathnanavod98:1998%23navod@cluster0.uxqik.mo
 }).then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log('Error connecting to MongoDB:', err));
 
-module.exports = mongoose;
+module.exports = mongoose; 
